c6462e42316e38483b4d00ec83554042995c4a42
chris.loh.jx <chris.loh.jx@35dac802-04e0-11df-af7f-9df10b504a6d>
2010-03-14 12:55:13 +0000

    Version 0.8

---
+++ trunk/SyncSharpV0.8/SyncSharp/ProgressForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SyncSharp.GUI
+{
+    public partial class ProgressForm : Form
+    {
+        public ProgressForm(string formTitle, string message, MainForm form)
+        {
+            InitializeComponent();
+            this.Text = formTitle;
+            this.Owner = form;
+            lblMessage.Text = message;
+        }
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        } 
+
+        public void SetMessage(string message)
+        {
+            lblMessage.Text = message;
+            lblMessage.Refresh();
+            progressBar.Refresh();
+        }
+
+        private void ProgressForm_Leave(object sender, EventArgs e)
+        {
+        }
+
+        private void ProgressForm_Load(object sender, EventArgs e)
+        {
+            this.CenterToParent();
+        }
+    }
+}
