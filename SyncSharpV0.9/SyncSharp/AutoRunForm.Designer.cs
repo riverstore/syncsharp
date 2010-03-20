62b2306f3d030ca0ede23c91f527fcad7207005c
jiayuan86 <jiayuan86@35dac802-04e0-11df-af7f-9df10b504a6d>
2010-03-20 17:23:16 +0000

    fix bug in AutoRunForm, amended UI for AboutBox form

--- SyncSharpV0.9/SyncSharp/AutoRunForm.Designer.cs
+++ SyncSharpV0.9/SyncSharp/AutoRunForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SyncSharp.GUI
+﻿using System.Windows.Forms;
+
+namespace SyncSharp.GUI
 {
     partial class AutoRunForm
     {
@@ -17,7 +19,19 @@
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
+        }
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
         }
 
         #region Windows Form Designer generated code
@@ -30,12 +44,6 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoRunForm));
-            this.scMain = new System.Windows.Forms.SplitContainer();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lvTaskList = new System.Windows.Forms.ListView();
-            this.colTaskName = new System.Windows.Forms.ColumnHeader();
-            this.colSource = new System.Windows.Forms.ColumnHeader();
-            this.colTarget = new System.Windows.Forms.ColumnHeader();
             this.btnDown = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
@@ -44,83 +52,21 @@
             this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
-            this.scMain.Panel1.SuspendLayout();
-            this.scMain.Panel2.SuspendLayout();
-            this.scMain.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.lvTaskList = new System.Windows.Forms.ListView();
+            this.colTaskName = new System.Windows.Forms.ColumnHeader();
+            this.colSource = new System.Windows.Forms.ColumnHeader();
+            this.colTarget = new System.Windows.Forms.ColumnHeader();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.statusBar.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // scMain
-            // 
-            this.scMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.scMain.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
-            this.scMain.IsSplitterFixed = true;
-            this.scMain.Location = new System.Drawing.Point(0, 0);
-            this.scMain.Name = "scMain";
-            // 
-            // scMain.Panel1
-            // 
-            this.scMain.Panel1.Controls.Add(this.groupBox1);
-            // 
-            // scMain.Panel2
-            // 
-            this.scMain.Panel2.Controls.Add(this.btnDown);
-            this.scMain.Panel2.Controls.Add(this.btnRemove);
-            this.scMain.Panel2.Controls.Add(this.btnCancel);
-            this.scMain.Panel2.Controls.Add(this.btnUp);
-            this.scMain.Size = new System.Drawing.Size(524, 302);
-            this.scMain.SplitterDistance = 475;
-            this.scMain.SplitterWidth = 2;
-            this.scMain.TabIndex = 0;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.lvTaskList);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(475, 302);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Task List";
-            // 
-            // lvTaskList
-            // 
-            this.lvTaskList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.colTaskName,
-            this.colSource,
-            this.colTarget});
-            this.lvTaskList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lvTaskList.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.lvTaskList.FullRowSelect = true;
-            this.lvTaskList.Location = new System.Drawing.Point(3, 17);
-            this.lvTaskList.MultiSelect = false;
-            this.lvTaskList.Name = "lvTaskList";
-            this.lvTaskList.Size = new System.Drawing.Size(469, 282);
-            this.lvTaskList.TabIndex = 0;
-            this.lvTaskList.UseCompatibleStateImageBehavior = false;
-            this.lvTaskList.View = System.Windows.Forms.View.Details;
-            // 
-            // colTaskName
-            // 
-            this.colTaskName.Text = "Task Name";
-            this.colTaskName.Width = 130;
-            // 
-            // colSource
-            // 
-            this.colSource.Text = "Source";
-            this.colSource.Width = 165;
-            // 
-            // colTarget
-            // 
-            this.colTarget.Text = "Target";
-            this.colTarget.Width = 165;
             // 
             // btnDown
             // 
             this.btnDown.Image = ((System.Drawing.Image)(resources.GetObject("btnDown.Image")));
-            this.btnDown.Location = new System.Drawing.Point(1, 102);
+            this.btnDown.Location = new System.Drawing.Point(8, 100);
             this.btnDown.Name = "btnDown";
             this.btnDown.Size = new System.Drawing.Size(39, 25);
             this.btnDown.TabIndex = 0;
@@ -132,7 +78,7 @@
             // btnRemove
             // 
             this.btnRemove.Image = ((System.Drawing.Image)(resources.GetObject("btnRemove.Image")));
-            this.btnRemove.Location = new System.Drawing.Point(1, 133);
+            this.btnRemove.Location = new System.Drawing.Point(8, 131);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(39, 25);
             this.btnRemove.TabIndex = 0;
@@ -144,7 +90,7 @@
             // btnCancel
             // 
             this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
-            this.btnCancel.Location = new System.Drawing.Point(1, 17);
+            this.btnCancel.Location = new System.Drawing.Point(8, 17);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(39, 25);
             this.btnCancel.TabIndex = 0;
@@ -156,7 +102,7 @@
             // btnUp
             // 
             this.btnUp.Image = ((System.Drawing.Image)(resources.GetObject("btnUp.Image")));
-            this.btnUp.Location = new System.Drawing.Point(1, 71);
+            this.btnUp.Location = new System.Drawing.Point(8, 69);
             this.btnUp.Name = "btnUp";
             this.btnUp.Size = new System.Drawing.Size(39, 25);
             this.btnUp.TabIndex = 0;
@@ -194,12 +140,66 @@
             this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
             this.progressBar.Visible = false;
             // 
+            // lvTaskList
+            // 
+            this.lvTaskList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colTaskName,
+            this.colSource,
+            this.colTarget});
+            this.lvTaskList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvTaskList.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lvTaskList.FullRowSelect = true;
+            this.lvTaskList.Location = new System.Drawing.Point(0, 0);
+            this.lvTaskList.MultiSelect = false;
+            this.lvTaskList.Name = "lvTaskList";
+            this.lvTaskList.Size = new System.Drawing.Size(465, 302);
+            this.lvTaskList.TabIndex = 2;
+            this.lvTaskList.UseCompatibleStateImageBehavior = false;
+            this.lvTaskList.View = System.Windows.Forms.View.Details;
+            // 
+            // colTaskName
+            // 
+            this.colTaskName.Text = "Task Name";
+            this.colTaskName.Width = 130;
+            // 
+            // colSource
+            // 
+            this.colSource.Text = "Source";
+            this.colSource.Width = 165;
+            // 
+            // colTarget
+            // 
+            this.colTarget.Text = "Target";
+            this.colTarget.Width = 165;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvTaskList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnRemove);
+            this.splitContainer1.Panel2.Controls.Add(this.btnDown);
+            this.splitContainer1.Panel2.Controls.Add(this.btnUp);
+            this.splitContainer1.Panel2.Controls.Add(this.btnCancel);
+            this.splitContainer1.Size = new System.Drawing.Size(524, 302);
+            this.splitContainer1.SplitterDistance = 465;
+            this.splitContainer1.SplitterWidth = 2;
+            this.splitContainer1.TabIndex = 3;
+            // 
             // AutoRunForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(524, 324);
-            this.Controls.Add(this.scMain);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusBar);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -210,12 +210,11 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SyncSharp PlugSync";
             this.Load += new System.EventHandler(this.AutoRunForm_Load);
-            this.scMain.Panel1.ResumeLayout(false);
-            this.scMain.Panel2.ResumeLayout(false);
-            this.scMain.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
             this.statusBar.ResumeLayout(false);
             this.statusBar.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,19 +222,18 @@
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer scMain;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.Button btnDown;
         private System.Windows.Forms.Button btnUp;
-        private System.Windows.Forms.ListView lvTaskList;
-        private System.Windows.Forms.ColumnHeader colTaskName;
-        private System.Windows.Forms.ColumnHeader colSource;
-        private System.Windows.Forms.ColumnHeader colTarget;
         private System.Windows.Forms.StatusStrip statusBar;
         private System.Windows.Forms.ToolStripStatusLabel lblStatus;
         private System.Windows.Forms.ToolTip toolTip;
         private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.ListView lvTaskList;
+        private System.Windows.Forms.ColumnHeader colTaskName;
+        private System.Windows.Forms.ColumnHeader colSource;
+        private System.Windows.Forms.ColumnHeader colTarget;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
