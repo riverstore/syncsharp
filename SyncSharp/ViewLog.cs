7d5f694b4d3ceac24c4d78d9e7e4dfcf450a03ee
jiayuan86 <jiayuan86@35dac802-04e0-11df-af7f-9df10b504a6d>
2010-04-01 05:01:21 +0000

    Tag V1.0

---
+++ SyncSharp/ViewLog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SyncSharp.Business;
+using System.IO;
+
+namespace SyncSharp.GUI
+{
+	public partial class ViewLog : Form
+	{
+		string logFile;
+
+		public ViewLog(string taskName, string logFile)
+		{
+			InitializeComponent();
+			this.Text = "[" + taskName + "]" + " log file";
+			this.logFile = logFile;
+		}
+
+		private void ViewLog_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				string contents = File.ReadAllText(logFile);
+				txtLog.Text = (contents == "") ? "Empty log file" : contents;
+				txtLog.Select(0, 0);
+			}
+			catch
+			{
+			}
+		}
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				File.WriteAllText(logFile, "");
+				txtLog.Clear();
+			}
+			catch
+			{
+			}
+		}
+	}
+}
