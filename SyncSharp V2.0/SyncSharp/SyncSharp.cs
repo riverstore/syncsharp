cca09e311f5c5cdb45297c50e77ad9c18d6d1c4a
chris.loh.jx <chris.loh.jx@35dac802-04e0-11df-af7f-9df10b504a6d>
2010-04-15 05:37:09 +0000

    SyncSharp V2.0 Final

---
+++ SyncSharp V2.0/SyncSharp/SyncSharp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using SyncSharp.GUI;
+using SyncSharp.Business;
+using SyncSharp.Storage;
+
+namespace SyncSharp
+{
+	static class SyncSharp
+	{
+        // Written by Guo Jiayuan and Loh Jianxiong Christoper 
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			try
+			{
+				SyncSharpLogic logic = new SyncSharpLogic();
+				logic.LoadProfile();
+				if (!logic.SaveProfile()) return;
+
+				logic.UpdateRemovableRoot();
+				bool runAutoForm = false;
+
+				if (logic.Profile.TaskCollection != null)
+				{
+					foreach (SyncTask task in logic.Profile.TaskCollection)
+					{
+						runAutoForm = task.Settings.PlugSync;
+						if (runAutoForm) break;
+					}
+				}
+
+				if (runAutoForm)
+				{
+					AutoRunForm autoRunFrm = new AutoRunForm(logic);
+					logic.AddUI(autoRunFrm);
+					Application.Run(autoRunFrm);
+				}
+				MainForm mainFrm = new MainForm(logic);
+				logic.RemoveAllUIs();
+				logic.AddUI(mainFrm);
+				Application.Run(mainFrm);
+			}
+			catch (Exception e)
+			{
+				Logger.WriteSystemErrorLog(e.StackTrace);
+			}
+		}
+	}
+}
---
+++ SyncSharp V2.0/SyncSharp/Utility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using SyncSharp.DataModel;
+
+namespace SyncSharp.Storage
+{
+    /// <summary>
+    /// Written by Guo Jiayuan, Loh Jianxiong Christoper, Tan YewKang 
+    /// </summary>
+	public static class Utility
+	{
+		public static String ComputeMyHash(FileUnit myFile)
+		{
+			FileStream myFileStream = new FileStream(myFile.AbsolutePath, FileMode.Open, FileAccess.Read);
+			String strHash = "";
+			if (myFile.Size < 30000)
+			{
+				byte[] tmpHash = new SHA1Managed().ComputeHash(myFileStream);
+				strHash = BitConverter.ToString(tmpHash, 0).Replace("-", "");
+			}
+			else
+			{
+				byte[] fileHeader = new byte[30000];
+				myFileStream.Read(fileHeader, 0, 30000);
+				byte[] tmpHash = new SHA1Managed().ComputeHash(fileHeader);
+				strHash = BitConverter.ToString(tmpHash, 0).Replace("-", "");
+			}
+			myFileStream.Dispose();
+			return strHash;
+		}
+
+        public static String FormatSize(long size)
+        {
+            string sizeString = "";
+            if (size < 1024)
+                sizeString = String.Format("{0:0.00 B}", size);
+            else if (size < 1048576)
+                sizeString = String.Format("{0:0.00 KB}", size / 1024.0);
+            else if (size < 1073741824)
+                sizeString = String.Format("{0:0.00 MB}", size / 1048576.0);
+            else
+                sizeString = String.Format("{0:0.00 GB}", size / 1073741824.0);
+            return sizeString;
+        }
+
+        public static void RenameMetaFiles(string oldName, string newName, string metaDataDir)
+        {
+            try
+            {
+                if (File.Exists(metaDataDir + @"\" + oldName + ".meta"))
+                    File.Move(metaDataDir + @"\" + oldName + ".meta",
+                            metaDataDir + @"\" + newName + ".meta");
+                if (File.Exists(metaDataDir + @"\" + oldName + ".log"))
+                    File.Move(metaDataDir + @"\" + oldName + ".log",
+                            metaDataDir + @"\" + newName + ".log");
+                if (File.Exists(metaDataDir + @"\" + oldName + ".bkp"))
+                    File.Move(metaDataDir + @"\" + oldName + ".bkp",
+                            metaDataDir + @"\" + newName + ".bkp");
+            }
+            catch
+            {
+            }
+        }
+	}
+}
